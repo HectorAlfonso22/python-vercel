1a2689571d71d27f7a38cf3789d7a289a171be62
hector_alfonso <hectorbog30@gmail.com>
2024-11-19 21:12:34 -0300

    2

--- proyect/settings.py
+++ proyect/settings.py
@@ -83,16 +83,10 @@ WSGI_APPLICATION = 'python_vercel_task.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+import os
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.hcxndzqqkcnepejpfpcx',
-        'PASSWORD': 'fMfsKbP$nWJ_X4A',
-        'HOST': 'aws-0-sa-east-1.pooler.supabase.com',
-        'PORT': '6543',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
